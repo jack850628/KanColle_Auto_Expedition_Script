ba22b42dce03252a5a1cfd5be36218bee20bdc50
奇幻 <30308931+jack850628@users.noreply.github.com>
2018-01-05 23:32:20 +0800

    修正dll路徑

--- CheckCursorType/CheckCursorType.java
+++ CheckCursorType/CheckCursorType.java
@@ -1,9 +1,13 @@
+import java.io.File;
+import java.io.IOException;
 import AShell.Data_Type_And_Struct.AShell_this;
 import AShell.ValueProcess.AShellType;
 
 public class CheckCursorType{
 	static{
-		System.load("C:\\Users\\jack8\\Desktop\\Ä¥¶¤¸}¥»\\CheckCursorType\\check_mouse_cursor_type.dll");
+		try{
+			System.load(new File(CheckCursorType.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getCanonicalPath()+"\\check_mouse_cursor_type.dll");
+		}catch(IOException e){}
 	}
 	public static native boolean CheckCursorType(int type);
 	public static AShellType getCheckCursorType(AShell_this AShthis,AShellType[] Args)throws Exception{
