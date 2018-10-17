2a68f03deebb5157c7d8154e52c441f57ff9a9c5
奇幻 <30308931+jack850628@users.noreply.github.com>
2018-10-17 15:26:23 +0800

    通知器

---
+++ KanColle_Remind/KanColle_Remind/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace KanColle_Remind {
+    partial class Form1 {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "艦隊即將回港";
+            this.notifyIcon1.BalloonTipTitle = "注意";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(120, 0);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "KanColle_Remind";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ KanColle_Remind/KanColle_Remind/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KanColle_Remind {
+    public partial class Form1 : Form {
+        public Form1(string[] args) {
+            InitializeComponent();
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+            this.Hide();
+            if(args.Length>0)
+                notifyIcon1.BalloonTipTitle = args[0];
+            if(args.Length > 1)
+                notifyIcon1.BalloonTipText = args[1];
+            notifyIcon1.ShowBalloonTip(3000);
+            if(args.Length > 2)
+                SoundPlaye(args[2]);
+            else {
+                Thread.Sleep(3000);
+                System.Environment.Exit(System.Environment.ExitCode);
+            }
+        }
+
+        private void SoundPlaye(string path) {
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.PlayStateChange += new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(wplayer_PlayStateChange);//設定播放監聽處理
+            wplayer.URL = path;//指定音樂路徑
+            wplayer.uiMode = "invisible";//設定為不顯示播放畫面
+            wplayer.controls.play();//播放開始
+        }
+
+        private void wplayer_PlayStateChange(int NewState) {
+            this.Visible = false;//將視窗顯示設定為不顯示
+            if(NewState == (int)WMPLib.WMPPlayState.wmppsMediaEnded) {//如果NewState等於播放結束
+                System.Environment.Exit(System.Environment.ExitCode);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) {
+
+        }
+    }
+}
